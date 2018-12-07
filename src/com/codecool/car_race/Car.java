0f0e1fe24a0eedb41200f790af656a818b339da7
Thao Phuong Lam <lphuongt930906@gmail.com>
2018-12-07 20:10:22 +0100

    constructor for Car's normalSpeed & name

--- src/com/codecool/car_race/Car.java
+++ src/com/codecool/car_race/Car.java
@@ -3,21 +3,33 @@ import java.util.Random;
 
 public class Car extends Vehicle {
     Integer normalSpeed;
-    String name;
-    Integer distanceTraveled;
-
     Random randSpeed = new Random();
 
+
+    String name;
     String[] carNames =
             {"Avalanche", "Crown", "Silver", "Evolution", "Stardust",
                     "Temperament", "Hero", "Daydream", "Supremacy", "Tarragon"};
+    Random randCarIndex = new Random();
+    Integer oneCarNameIndex = randCarIndex.nextInt(10) + 1;
+    String oneCarName = carNames[oneCarNameIndex];
+    Integer otherCarNameIndex = randCarIndex.nextInt(10) + 1;
+    String otherCarName = carNames[otherCarNameIndex];
+
+
+    Integer distanceTraveled;
 
     public Car() {
         this.normalSpeed = randSpeed.nextInt(110) + 80;
+        this.name = oneCarName + otherCarName;
     }
 
     public String getNormalSpeed() {
         return normalSpeed.toString() + "km/h";
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void prepareForLap(Race race) {
