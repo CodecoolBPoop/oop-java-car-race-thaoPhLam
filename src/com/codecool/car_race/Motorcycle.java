4198429590eb7c5d0d5df50440890e6bcc3c5826
Thao Phuong Lam <lphuongt930906@gmail.com>
2018-12-25 14:05:30 +0100

    deleted all for a new start

--- src/com/codecool/car_race/Motorcycle.java
+++ src/com/codecool/car_race/Motorcycle.java
@@ -2,38 +2,5 @@ package com.codecool.car_race;
 import java.util.Random;
 
 public class Motorcycle extends Vehicle {
-    Integer motorcycleNumber;
-    String name;
-    Integer motorCounter = 1;
 
-    Integer distanceTraveled;
-
-    public Motorcycle() {
-        for (int i = 0; i < 11; i++) {
-            this.motorcycleNumber = i;
-            this.name = "Motorcycle " + motorcycleNumber;
-        }
-        /*
-        this.motorcycleNumber = motorCounter;
-        this.name = "Motorcycle " + motorcycleNumber;
-        motorCounter++;
-        */
-        System.out.println(this.name);
-    }
-
-    public Integer getMotorcycleNumber() {
-        return motorcycleNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void prepareForLap(Race race) {
-
-    }
-
-    public void moveForAnHour() {
-
-    }
 }
--- src/com/codecool/car_race/Truck.java
+++ src/com/codecool/car_race/Truck.java
@@ -2,13 +2,5 @@ package com.codecool.car_race;
 import java.util.Random;
 
 public class Truck extends Vehicle {
-    Integer breakdownTurnsLeft;
-    Integer distanceTraveled;
-    public void prepareForLap(Race race) {
 
-    }
-
-    public void moveForAnHour() {
-
-    }
 }
